0c50700efee5056f6def40f380618d88a4fd99c2
Denis Kramer <d.kramer@soton.ac.uk>
2018-05-05 23:50:02 +0100

    Removed termination of slim with x11vnc

--- assets/x11vnc-session.sh
+++ assets/x11vnc-session.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 /usr/bin/x11vnc -xkb -noxrecord -noxfixes -noxdamage -display :0 -repeat -nopw -wait 5 -permitfiletransfer -tightfilexfer -auth /var/run/slim.auth
-/usr/bin/supervisorctl restart slim
+#/usr/bin/supervisorctl restart slim
